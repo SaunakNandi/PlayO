376655ad37e56819b023d2e60728e521d004d0bc
SaunakNandi <saunaknandi021000@gmail.com>
2025-04-03 09:41:28 +0530

    Mandatory checkbox implemented and PasswordScreen skeleton done functionality left

--- screens/PasswordScreen.js
+++ screens/PasswordScreen.js
@@ -1,9 +1,15 @@
-import { StyleSheet, Text, View,Image } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View,Image, TextInput, TouchableOpacity } from 'react-native'
+import React,{useState} from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import AntDesign from 'react-native-vector-icons/AntDesign'
-
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+import { useNavigation } from '@react-navigation/native'
 const PasswordScreen = () => {
+  const [password,setPassword] =useState()
+  const navigation=useNavigation()
+  function handleNext(){
+    navigation.navigate("Name")
+  }
   return (
     <SafeAreaView style={{flex: 1, backgroundColor: 'white'}}>
       <View style={{marginTop: 90, marginHorizontal: 20}}>
@@ -20,6 +26,14 @@ const PasswordScreen = () => {
         </View>
         <Text style={{fontSize: 25,fontWeight: 'bold',fontFamily: 'GeezaPro-Bold',
           marginTop: 15}}>Please choose a password</Text>
+          <TextInput  placeholder='Enter your password' secureTextEntry={true} autoFocus={true}
+          onChangeText={text=>setPassword(text)}
+          style={{width: 340,marginVertical: 10,marginTop: 25,borderBottomColor: 'black',borderBottomWidth: 1,paddingBottom: 10,fontFamily: 'GeezaPro-Bold',fontSize: 22}}/>
+          <Text style={{color: 'gray', fontSize: 15, marginTop: 7}}>Note: You details will be safe with us</Text>
+          <TouchableOpacity activeOpacity={0.8} onPress={handleNext} style={{marginTop:30,marginLeft:"auto"}}>
+            <MaterialCommunityIcons color='green' size={45} name='arrow-right-circle'
+            style={{alignSelf:"center",marginTop:20}}/>
+          </TouchableOpacity>
       </View>
     </SafeAreaView>
   )
--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -8,6 +8,12 @@ const RegisterScreen = () => {
   const [email,setEmail]=useState('')
   const [checked,setChecked] = useState(false)
   const navigation=useNavigation()
+  const [nextDisabled,setNextDisabled]=useState(true)
+  const checkboxClicked=()=>{
+    setChecked(prev=>!prev)
+    console.log(nextDisabled)
+    setNextDisabled(!nextDisabled)
+  }
   return (
     <SafeAreaView>
       <View style={{padding: 13}}>
@@ -18,16 +24,16 @@ const RegisterScreen = () => {
           style={{padding: 15,borderColor: '#D0D0D0',borderWidth: 1,borderRadius: 10}}/>
 
           <Pressable style={{padding:15, borderRadius: 8,backgroundColor:email?.length>4? '#2dcf30':'#E0E0E0'}}
-          onPress={()=>navigation.navigate("Password")}>
+          onPress={()=>navigation.navigate("Password")} disabled={!checked}>
+            {/* {console.log(!checked)} */}
             <Text style={{textAlign: 'center'}}>Next</Text>
           </Pressable>
         </View>
         <View style={{flexDirection:'row',justifyContent: 'center', alignItems: 'center'}}>
           {/* <Text style={{textAlign: 'center', fontWeight: '500', fontSize: 15}}>I agree to recieve updates over Whatsapp</Text> */}
-          <Pressable style={{width:18,height:18,borderColor:'gray',borderRadius:5,borderWidth:2,alignItems:'center',justifyContent:'center',marginLeft: 5, position: 'relative',padding:0,zIndex:1 }} onPress={()=>setChecked(prev=>!prev)}>
-            </Pressable>
+          <Pressable style={{width:18,height:18,borderColor:'gray',borderRadius:5,borderWidth:2,alignItems:'center',justifyContent:'center',marginLeft: 5, position: 'relative',padding:0,zIndex:1 }} onPress={()=>setChecked(prev=>!prev)}/>
             {checked && <MaterialIcons name='check' size={26} color='black'
-            onPress={()=>setChecked(prev=>!prev)} style={{position:'absolute',left:-10}}/>}
+            style={{position:'absolute',left:-10}}/>}
           <Text style={{fontSize: 15,color: 'gray',textAlign: 'center',marginTop: 20}}>By Signing up, you agree to the terms of services and privacy policy</Text>
         </View>
       </View>
